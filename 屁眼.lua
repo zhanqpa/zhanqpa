6d6a3f38f3b5d34e3d66da04eb33bc0be4ee7def
zhanqpa <zhanqpa@qq.com>
2024-10-11 20:13:11 +0800

    Update 屁眼.lua

--- 屁眼.lua
+++ 屁眼.lua
@@ -210,6 +210,33 @@ Tab:AddToggle({
 
 Tab:AddButton({
 
+	Name = "夜视加强版",
+
+	Callback = function()
+
+local Light = game:GetService("Lighting")
+
+function dofullbright()
+Light.Ambient = Color3.new(1, 1, 1)
+Light.ColorShift_Bottom = Color3.new(1, 1, 1)
+Light.ColorShift_Top = Color3.new(1, 1, 1)
+end
+
+dofullbright()
+
+Light.LightingChanged:Connect(dofullbright)
+
+    end
+})
+
+dofullbright()
+
+Light.LightingChanged:Connect(dofullbright)
+
+    end
+})
+Tab:AddButton({
+
 	Name = "穿墙UI",
 
 	Callback = function()
